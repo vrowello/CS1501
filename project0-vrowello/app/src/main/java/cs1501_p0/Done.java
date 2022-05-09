f7207ec68b7884116fa29440ce79be757ed57c9d
= <=>
2022-05-09 11:13:44 -0400

    Initial Commit

--- project0-vrowello/app/src/main/java/cs1501_p0/Done.java
+++
@@ -1,7 +0,0 @@
-package cs1501_p0;
-
-public class Done {
-	public static void main(String[] args) {
-		System.out.println("DONE");
-	}
-}
--- project0-vrowello/app/src/main/java/cs1501_p0/Extra.java
+++
@@ -1,7 +0,0 @@
-package cs1501_p0;
-public class main {
-	public static void extra()  {
-		System.out.println("I am currently taking CS1501 and doing project zero");
-	}
-}
-
